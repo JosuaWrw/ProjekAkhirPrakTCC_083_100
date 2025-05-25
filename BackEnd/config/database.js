bbe5b93777272e3868f39c54d099303e759870bd
JosuaWrw <joshuaw2268@gmail.com>
2025-05-25 16:37:36 +0700

    update .env

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -1,7 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('dealer_db','root','',{
-    host: 'localhost',
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD,{
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
