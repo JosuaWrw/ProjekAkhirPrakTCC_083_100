63bb525fc689f5babe71a17fccaf3b0a8d3b9951
JosuaWrw <joshuaw2268@gmail.com>
2025-05-25 17:53:57 +0700

    perbarui index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ['http://127.0.0.1:5500', 'http://localhost:5500'] }));
+app.use(cors({ credentials: true, origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://fe-projek-dot-alert-parsec-450813-a1.et.r.appspot.com'] }));
 app.use(express.json());
 app.use(route);
 
